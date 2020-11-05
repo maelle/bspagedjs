b2e0e0c8e2533b13a6b58695abb9afdba958d019
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-11-05 13:47:26 +0100

    more

--- build.R
+++ build.R
@@ -1,7 +1,10 @@
 bookdown::render_book(".")
 
-page_path <- "docs/intro.html"
 
+if (fs::dir_exists("docs2")) fs::dir_delete("docs2")
+fs::dir_copy("docs", "docs2")
+
+htmls <- fs::dir_ls("docs2", glob = "*.html")
 tweak_page <- function(page_path) {
   # put TOC at the begging
   page <- xml2::read_html(page_path)
@@ -20,6 +23,30 @@ tweak_page <- function(page_path) {
   xml2::write_html(page, page_path)
 }
 
-tweak_page(page_path)
+lapply(htmls, tweak_page)
 
-system("pagedjs-cli ./docs/intro.html -o docs/result.pdf")
+page1 <- xml2::read_html("docs2/index.html")
+
+get_contents <- function(page_path, main = xml2::xml_find_first(page1, ".//main[@id='content']")) {
+  page <- xml2::read_html(file.path("docs2", basename(page_path)))
+  contents <-  xml2::xml_contents(xml2::xml_find_first(page, ".//main[@id='content']"))
+
+  for (i in contents) {
+    xml2::xml_add_child(main, i)
+  }
+}
+
+ordered_htmls <- xml2::xml_attr(
+  xml2::xml_find_all(page1, ".//ul[contains(@class, 'book-toc')]//li//a"),
+  "href"
+)
+ordered_htmls <- ordered_htmls[ordered_htmls!="index.html"]
+ordered_htmls <- file.path("docs2", ordered_htmls)
+
+lapply(ordered_htmls, get_contents)
+
+
+xml2::write_html(page1, "docs2/index.html")
+
+
+system("pagedjs-cli ./docs2/index.html -o docs/result.pdf")
